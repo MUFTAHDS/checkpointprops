68158da89916ed1b98029d40ba139995fe892ce8
MUFTAHDSا <balraslymftah@gmail.com>
2021-12-01 12:41:50 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,73 @@
-import Profile from "./profile1/profile.js";
-import Photo from "./profile1/R.png";
-import "./App.css";
-export default function App() {
-    var x = "imen soltani";
-    const funprop = () => {
-        return alert("أحذر هنالك خطر ");
-    };
-    return (
-        <div className="App">
-            <Profile OLI="MY MUFTAH66" Photo={Photo} alert={funprop} />
-        </div>
-    );
+import React, { Component } from "react";
+import './App.css';
+
+export default class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            src: "https://images.prismic.io/mystique/5d7c09b9-40e5-4254-ae1c-2c1cb59aa898_IMG3.jpg?auto=compress,format",
+            counter: 0
+
+        }
+    }
+
+
+
+
+
+
+
+    increse = () => {
+        this.setState({
+            counter: this.state.counter + 1
+        })
+    }
+    decrese = () => {
+        this.setState({
+            counter: this.state.counter - 1
+        })
+    }
+
+    muftah = () => {
+        this.setState({
+            counter: 0
+        })
+
+    }
+
+    decrese = () => {
+        if (this.state.counter > 0) {
+            this.setState({
+                counter: this.state.counter - 1
+            });
+        }
+    }
+
+
+    render() {
+        return (
+            <div className="vv">
+                <h1>سبايدرمان أو الرجل العنكبوت شخصية خيالية خارقة </h1>
+
+                <p> {this.state.counter} </p>
+
+                <img className="kk" src={this.state.src} alt="img in src" />
+
+                <div className="mn">
+                    <button onClick={this.increse}  >+</button>
+                    <button onClick={this.decrese}>-</button>
+                    <button onClick={this.muftah}>reset</button>
+                </div>
+
+
+            </div>
+        )
+    }
+
+
+
+
+
+
 }
